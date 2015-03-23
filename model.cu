87ae387058edd9f97f4eb193abbf81f2dc8b7950
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-03-22 22:00:11 -0600

    Resultados

--- model.cu
+++ model.cu
@@ -361,7 +361,44 @@ void model5_open(double *dev_R, double *dev_I, itpp::vec js, double j, double jp
     Uk_kernel<<<numblocks,numthreads>>>(i,dev_R,dev_I,bx,by,bz,kcos,ksin,l);     
     }
   return;  
-  }  
+  }
+  
+void model6(double *dev_R, double *dev_I, itpp::vec js, double j, double jp, itpp::mat b , int nqubits, int xlen){ 
+  /*    MODEL 6 CLOSED
+ <-*   *   *   *   *   *  ->    
+    \     /     \     /
+  <- *   *   *   *   *   *  ->
+      \       \     /     \
+   <-  *   *   *   *   *   * ->
+             \      
+              *  last qubit
+          
+  */
+  int numthreads, numblocks;
+  double kcos,ksin,bx,by,bz;
+  int l=pow(2,nqubits);
+  choosenumblocks(l,numthreads,numblocks);
+  //la evolucion de la cadena A de tamaño xlen
+  for(int i=0;i<xlen;i++) {
+    Ui_kernel<<<numblocks,numthreads>>>(i,(i+1)%(xlen),dev_R,dev_I,cos(js(i)),sin(js(i)),l);
+    }  
+  //la evolucion de la cadena B de tamaño nqubits - xlen - 1  
+  for(int i=0;i<nqubits-1-xlen;i++) {
+    Ui_kernel<<<numblocks,numthreads>>>(i+xlen,(i+1)%(nqubits-1-xlen)+xlen,dev_R,dev_I,cos(js(i)),sin(js(i)),l);
+    }
+  //la interaccion A B es 1 a 1 
+  for(int i=0;i<xlen-1;i++) {
+    Ui_kernel<<<numblocks,numthreads>>>(i,i+xlen,dev_R,dev_I,cos(jp),sin(jp),l);
+    } 
+  //se hace la interacion 0 con A
+  Ui_kernel<<<numblocks,numthreads>>>(nqubits-1,0,dev_R,dev_I,cos(j),sin(j),l);
+  //evolucion patada magnetica
+  for(int i=0;i<nqubits;i++) {
+    set_parameters(b.get_row(i),kcos,ksin,bx,by,bz);
+    Uk_kernel<<<numblocks,numthreads>>>(i,dev_R,dev_I,bx,by,bz,kcos,ksin,l);     
+    }
+  return;  
+  }   
   
     
     
