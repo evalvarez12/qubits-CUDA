11799071ece734e1f2ad8d111bbbcfdaf9210f9c
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-07-07 23:01:17 -0500

    Figs

--- g1-2.py
+++ g1-2.py
@@ -27,50 +27,6 @@ import nodos as nod
 
 
 
-fig=figure()
-title('$b \perp J$',fontsize=28)
-
-xticks([])
-yticks([])
-
-
-fig.add_subplot(2,1,1)
-
-
-
-
-conx=loadtxt("conexionesABC7.txt")
-conx=conx[1:]
-lines=['o-','<-','p-','v-','*-','^-','D-','h-','>-','H-','d-','x-','s-','+-']
-labels=['$\gamma=5\lambda$','$\gamma=10\lambda$','$\gamma=30\lambda$','$\gamma=50\lambda$','$\gamma=80\lambda$','$\gamma=100\lambda$','$\gamma=140\lambda$']
-for i in range(1,7) :
-  a=loadtxt("grafica1-6-"+str(i)+".dat")
-  plot(a,lines[i],markevery=60,label=labels[i-1])
-
-
-xlabel("$t$",fontsize=28)
-#xticks(log(arange(0.000001,.1+.001,.05)),('$0$','$0.05$','$0.1$'),fontsize=15)
-xticks((500,1000,1500,2000,2500),('$500$','$1000$','$1500$','$2000$','$2500$'),fontsize=25)
-yticks((1,0.9,0.8,0.7,0.6,0.5),('$1$','$0.9$','$0.8$','$0.7$','$0.6$','$0.5$'),fontsize=25)
-ylabel("$P$",fontsize=28)
-#text(2000,0.7,'$b \perp J$',fontsize=28)
-
-legend(loc='lower left',fontsize=25)
-
-tick_params(axis='both',          # changes apply to the x-axis
-    which='both',      # both major and minor ticks are affected
-    bottom='on',      # ticks along the bottom edge are off
-    top='on',         # ticks along the top edge are off
-    labelbottom='on',
-    length=10)
-
-#INSET
-#ax = axes([.55, .65, .33, .22])
-ax = axes([.55, .1, .33, .22])
-nod.nodosABC(conx,'-')
-
-
-fig.add_subplot(2,1,2)
 
 
 conx=loadtxt("conexionesABC-2-6.txt")
