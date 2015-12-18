18e093a384be0b07019f9c3cb3cbc7ae8b654c16
Eduardo Villaseñor <evalvarez12@gmail.com>
2015-12-18 13:38:15 -0600

    Figures

--- g1-2.py
+++ g1-2.py
@@ -47,7 +47,7 @@ fig=figure(figsize=(9, 6)) # in inches!
 
 conx=loadtxt("conexionesABC7.txt")
 conx=conx[1:]
-lines=['s-','<-','p-','v-','D-','H-','D-','h-','>-','H-','d-','x-','s-','+-']
+lines=['bs-','g<-','rp-','cv-','mD-','yH-','D-','h-','>-','H-','d-','x-','s-','+-']
 labels=['$\gamma=5\lambda$','$\gamma=10\lambda$','$\gamma=30\lambda$','$\gamma=50\lambda$','$\gamma=80\lambda$','$\gamma=100\lambda$','$\gamma=140\lambda$']
 
 
@@ -56,8 +56,8 @@ labels=['$\gamma=5\lambda$','$\gamma=10\lambda$','$\gamma=30\lambda$','$\gamma=5
 
 #labels=['$N_e=6$','$N_e=9$','$N_e=12$','$N_e=15$','$\gamma=0.5$','$\gamma=1.$','$\gamma=140$']
 for i in range(1,7) :
-  a=loadtxt("grafica1-3-Cpatead2"+str(i)+".dat")
-  plot(a,lines[i],markevery=60,label=labels[i-1],markersize=10)
+  a=loadtxt("grafica1-3-"+str(i)+".dat")
+  plot(a,lines[i-1],markevery=60,label=labels[i-1],markersize=10)
 
 
 xlabel("$t$",fontsize=17)
@@ -84,4 +84,4 @@ ax = axes([.28, .65, .43, .22])
 #ax = axes([.55, .1, .33, .22])
 nod.nodosABC_lamb_gamma_labels(conx,'-')
 
-savefig('C-pateado2.png')
+savefig('art-g1-3.png')
